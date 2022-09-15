18b6a763a392576722475b0b1a64038b8c69a2cd
Francoc7 <53262338+Francoc7@users.noreply.github.com>
2022-09-15 18:30:42 -0300

    First Exercice

---
+++ Ejercicio1Ejemplos/Ejercicio1Ejemplos/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio1Ejemplos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+                Console.WriteLine("Ingrese su nombre");
+                String nombre = Console.ReadLine();
+                Console.ReadKey();
+
+                Console.WriteLine("Ingrese un Pais");
+                String pais = Console.ReadLine();
+                Console.ReadKey();
+
+                Console.WriteLine("Hola " + nombre + " Bienvenido a " + pais);
+                Console.ReadKey();
+
+        }
+    }
+}
