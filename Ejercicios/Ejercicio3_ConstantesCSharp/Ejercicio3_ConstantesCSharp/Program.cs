0ddff0e94a71a8529af42ea5c9222e2354bdf77d
Francoc7 <53262338+Francoc7@users.noreply.github.com>
2022-09-18 11:48:44 -0300

    Ejercicios Nuevos + Teoria

---
+++ Ejercicios/Ejercicio3_ConstantesCSharp/Ejercicio3_ConstantesCSharp/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio3_ConstantesCSharp
+{
+    internal class Program
+    {
+        //Constantes, la constante es el " const " que se agrega entre la entrada y el tipo de dato, y este sirve para que el valor que se ingresa sea constantes en todo el programa sin poder modificar ese valor
+        public const string NOMBRE = "FRANCO";
+        public const int NUMERO_PEDIDOS_MAXIMOS = 10;
+
+        static void Main(string[] args)
+        {
+            //Ejemplo de uso de constante.
+
+            for (int i = 0; i < 12; i++)
+            {
+                if (i < NUMERO_PEDIDOS_MAXIMOS)
+                {
+                    Console.WriteLine(i);
+                    Console.ReadKey();
+                }
+            }
+
+        }
+    }
+}
---
+++ Ejercicios/Ejercico2_NomenclaturaCSharp/Ejercico2_NomenclaturaCSharp/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercico2_NomenclaturaCSharp
+{
+    internal class Program
+    {
+        //Variable privada, siempre tiene que empezar con " _ "
+        private readonly string _varPrivada;
+        static void Main(string[] args)
+        {
+            //Camel Case, empieza en minuscula y las siguientes palabras en mayusculas, se usa en variables locales y en mienbros privados. 
+            var estoEsCamel = "Camel";
+
+            //Pascal Case, se empeiza en mayusucla y las palabras siguientes siguen empezandon en mayusuclas, se usan en metodos, clases y tipos.
+            var EstoEsPascal = "Pascal";
+        }
+
+        //Ejemplo Correcto de metodo con Pascal y con estandares correctos
+
+        public int SumarDosNumeros(int Numero1, int Numero2)
+        {
+            var Resultado = Numero1 + Numero2;
+            return Resultado;
+
+        }
+
+        //Ejemplo de estandares Incorrectos
+        public int suma(int a,int b)
+        {
+            return a + b;
+        }
+
+    }
+}
