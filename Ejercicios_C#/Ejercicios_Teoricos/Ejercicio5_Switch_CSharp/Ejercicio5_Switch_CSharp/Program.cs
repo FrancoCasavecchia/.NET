4205e53b41287103f57a5dbf941be51bdfcc1ecf
Francoc7 <53262338+Francoc7@users.noreply.github.com>
2022-09-19 19:13:40 -0300

    Switch Exercice

---
+++ Ejercicios_C#/Ejercicios_Teoricos/Ejercicio5_Switch_CSharp/Ejercicio5_Switch_CSharp/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio5_Switch_CSharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //El swith tambien al igual que el if es un condicionante pero la diferencia es en su tipo de uso y en que el switch utiliza el "case" en vez del "else".
+
+            int number = 5;
+
+            switch (number)
+            {
+                case 5: Console.WriteLine("El numero es 5");
+                    break;
+
+                case 15:Console.WriteLine("No es 15");
+                    break;
+
+                case 78:Console.WriteLine("No es 79");
+                    break;
+
+                default: Console.WriteLine("No es ningun numero");
+                    break;
+            }
+
+        }
+    }
+}
