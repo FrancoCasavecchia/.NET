9dd324f3015e4bb0ca4c7a4203c7d9c475dbb7f3
Francoc7 <53262338+Francoc7@users.noreply.github.com>
2022-09-19 19:25:09 -0300

    DO-WHILE EXERCICE

---
+++ Ejercicios_C#/Ejercicios_Teoricos/Ejercicio6_DO-WHILE_CSharp/Ejercicio6_DO-WHILE_CSharp/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio6_DO_WHILE_CSharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //El DO WHILE ejecuta una intruccion o intrucciones minetras que una exprecion booleana sea true, esto se evalua luego de cada instruccion en el while por ende se ejecuta 1 o mas veces.
+
+            int number = 0;
+
+            do
+            {
+                //Esto se va a ejecutar hasta que number sea 10
+                Console.WriteLine(number);
+                number++;
+                Console.WriteLine("Se Sumo un numero");
+
+            } while (number <= 15);     //aca podemos ver el while que se va a ir repitiendo la intruccion hasta que la condicion se cumpla
+
+        }
+    }
+}
