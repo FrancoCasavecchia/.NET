ab4bfcd93ededb689af553e1218843a43e08a446
Francoc7 <53262338+Francoc7@users.noreply.github.com>
2022-09-26 18:20:49 -0300

    ARGS

---
+++ Ejercicios_C#/Ejercicios_Teoricos/Ejercicio11_IngresarYLeerArgumentos/Ejercicio11_IngresarYLeerArgumentos/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio11_IngresarYLeerArgumentos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hay " + args.Length + " Elementos");
+
+            foreach (var arg in args)
+            {
+                Console.WriteLine(arg);
+            }
+        }
+    }
+}
