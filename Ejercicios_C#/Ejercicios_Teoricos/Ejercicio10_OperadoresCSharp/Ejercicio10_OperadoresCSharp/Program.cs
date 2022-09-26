8d39b52e00b63ddf60fb8b536e81f803383be5c8
Francoc7 <53262338+Francoc7@users.noreply.github.com>
2022-09-26 17:52:21 -0300

    OPERADORES

---
+++ Ejercicios_C#/Ejercicios_Teoricos/Ejercicio10_OperadoresCSharp/Ejercicio10_OperadoresCSharp/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio10_OperadoresCSharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* OPERADORES 
+             * 
+             * DE CALCULO:
+             *  SUMA == " + "
+             *  RESTA == " - "
+             *  MULTIPLICACION == " * "
+             *  DIVISION == " / "
+             *  RESTO == " % "
+             *  
+             * DE COMPARACION
+             * IGUAL ==
+             * DISTINTO !=
+             * MENOR <
+             * MAYOR >
+             * MENOR O IGUAL <=
+             * MAYOR O IGUAL >=
+             * 
+             * LOGICOS
+             * 
+             * Y/AND == &&
+             * O/OR == ||
+             */ 
+        }
+    }
+}
